a935cf1a0c529eed07f31560c004d4ab872393fe
KAUA GABRIEL FERREIRA BARBOZA <46259062885@info.local>
2024-05-09 10:54:56 -0300

    feat: Exercicio 6

---
+++ Ex6.js
@@ -0,0 +1,16 @@
+// Exercício 6: Verificação de idade
+// Escreva um programa que solicita o ano de nascimento de uma pessoa e verifica se ela é maior
+// de 18 anos. Exiba uma mensagem indicando se a pessoa é maior de idade ou não.
+
+alert("Seja bem-vindo")
+
+let anodenascimento = prompt("Em que ano você nasceu?")
+let mesdenascimento = prompt("Em que mês você nasceu? (Em número por gentileza)")
+
+if(anodenascimento>=2006 && mesdenascimento<=5 );
+    alert("Olha você é maior de 18, parabéns a vida adulta chama");
+
+if(anodenascimento>2006 && mesdenascimento>=6);
+    alert("Hmm... parece que você tem menos de 18 anos , aproveite a vida amiguinho");
+
+
